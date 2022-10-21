a3c55a3d3ad998605ec967f743829bd466be84d4
Willian <willian.alves.b15@gmail.com>
2022-10-21 19:20:30 -0300

    feat: created file_management

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,11 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        sys.stderr.write("Formato inválido\n")
+    try:
+        with open(path_file, mode="r") as Arquivo:
+            return Arquivo.read().split("\n")
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
