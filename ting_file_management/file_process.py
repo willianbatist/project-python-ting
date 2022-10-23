16a650f2723c3ae4236fbcd7f17b2a2ee91ac9eb
Willian <willian.alves.b15@gmail.com>
2022-10-23 19:25:47 -0300

    feat: criando implementando process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,19 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for fileira in instance._data:
+        if fileira["nome_do_arquivo"] == path_file:
+            return
+    texto = txt_importer(path_file)
+    objeto = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(texto),
+        "linhas_do_arquivo": texto
+    }
+    instance.enqueue(objeto)
+    sys.stdout.write(str(objeto))
 
 
 def remove(instance):
