5570e093795e71537fa44bdeb7e0a54e0cfab052
Willian <willian.alves.b15@gmail.com>
2022-10-24 17:49:15 -0300

    feat: implementando remove

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -17,7 +17,12 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) < 1:
+        sys.stdout.write("Não há elementos\n")
+        return
+    removendo_arquivo = instance.dequeue()["nome_do_arquivo"]
+    sys.stdout.write(f"Arquivo {removendo_arquivo} removido com sucesso\n")
+    return
 
 
 def file_metadata(instance, position):
