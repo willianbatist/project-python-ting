8571da18b814110ae6c5a96c1e494f97287d29f9
Willian <willian.alves.b15@gmail.com>
2022-10-25 10:34:23 -0300

    feat: implementando

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    for fileira in range(instance.__len__()):
+        arquivo = instance.search(fileira)
+        lista_ocorrencia = []
+        i = 1
+        for fileira, linha in enumerate(arquivo["linhas_do_arquivo"]):
+            if word.lower() in linha.lower():
+                lista_ocorrencia.append({"linha": i})
+            i += 1
+        lista_de_palavras = []
+        resultado = {}
+        total_ocorrencia = len(lista_ocorrencia)
+        if total_ocorrencia > 0:
+            resultado["palavra"] = word
+            resultado["arquivo"] = arquivo["nome_do_arquivo"]
+            resultado["ocorrencias"] = lista_ocorrencia
+            lista_de_palavras.append(resultado)
+    return lista_de_palavras
 
 
 def search_by_word(word, instance):
